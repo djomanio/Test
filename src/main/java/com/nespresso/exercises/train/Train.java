c9d3afdb2ed655122bef6063a82eb17014ca8899
Jordi Rolf <rolf.jordi@nespresso.com>
2013-06-07 10:17:27 +0100

    initial commit

---
+++ src/main/java/com/nespresso/exercises/train/Train.java
@@ -0,0 +1,31 @@
+package com.nespresso.exercises.train;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Train {
+
+	public Train(String name) {
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public String printTrain(String name){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public String print(){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public void detachEnd(){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public void detachHead(){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public boolean fill(){
+        throw new UnsupportedOperationException("Still to be implemented");
+	}
+}
